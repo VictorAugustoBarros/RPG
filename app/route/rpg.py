3cd481993fb4f73b60b1d9f8233efddf50fa51c4
victor <victor.barros@pgmais.com.br>
2020-02-04 16:26:29 -0300

    oks

--- app/route/rpg.py
+++ app/route/rpg.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask
 from flask import Blueprint
 from flask import request
@@ -6,6 +7,8 @@ from utils.schema import Schema
 from utils.request_log import RequestLog
 from utils.authorization import Authorization
 from service.sms.v1.company import Company
+from flasgger.utils import swag_from
+from flask import Flask, jsonify, request
 
 base_url = "/api/v1/sms"
 application = Flask(__name__)
@@ -13,9 +16,19 @@ rpg_blueprint = Blueprint("sms", __name__, url_prefix=base_url)
 schema = Schema()
 
 
-@rpg_blueprint.route("/company/<uid>/range/limit/<start_date>/<end_date>", methods=["GET"])
-@RequestLog.register
-@Authorization.auth
-@swag_from(schema.company_range_limit("SMS"))
-def sms_company_range_limit(uid, start_date, end_date):
-    return Company.get_time_range_limit(uid, start_date, end_date)
+@rpg_blueprint.route('/api/<string:language>/', methods=['GET'])
+@swag_from('index.yml')
+def index(language):
+    language = language.lower().strip()
+    features = [
+        "awesome", "great", "dynamic",
+        "simple", "powerful", "amazing",
+        "perfect", "beauty", "lovely"
+    ]
+    size = int(request.args.get('size', 1))
+    if language in ['php', 'vb', 'visualbasic', 'actionscript']:
+        return "An error occurred, invalid language for awesomeness", 500
+    return jsonify(
+        language=language,
+        features=random.sample(features, size)
+    )
