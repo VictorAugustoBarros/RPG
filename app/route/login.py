a7c3c48a0380e70ab587bd4f473ff93122ae5862
victor <victor.barros@pgmais.com.br>
2020-02-05 18:01:24 -0300

    testes

--- app/route/login.py
+++ app/route/login.py
@@ -10,7 +10,13 @@ login_blueprint = Blueprint("login", __name__, url_prefix=base_url)
 schema = Schema()
 
 
-@login_blueprint.route('/new_player/<email>/<login>/<senha>/<nick>/', methods=['POST'])
+@login_blueprint.route('/new_account/<email>/<login>/<senha>/<nick>/', methods=['POST'])
 @swag_from('details_user/new_user.yml')
-def index(email, login, senha, nick):
+def new_account(email, login, senha, nick):
+    return {"success": True}
+
+
+@login_blueprint.route('/delete_account/<email>/<login>/<senha>/', methods=['DELETE'])
+@swag_from('details_user/new_user.yml')
+def delete_account(email, login, senha):
     return {"success": True}
